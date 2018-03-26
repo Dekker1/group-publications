0793656ab976c7bc7fe8585226ce8605b3ab34c7
Jip J. Dekker <jip@dekker.li>
2018-03-26 13:19:35 +1100

    Add initial script to output publications of a group

---
+++ group_citations.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from api import scholar
+import datetime
+import csv
+import sys
+
+current = datetime.datetime.now().year
+
+if len(sys.argv) < 2:
+    print("No list of people Provided")
+    exit(1)
+
+people = []
+articles = []
+
+with open(sys.argv[1], newline='\n') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in reader:
+        length = len(row)
+        assert(length >= 3)
+        for name in range(length-2):
+            start = current if row[length-2] == "current" else int(row[length-2])
+            end = current if row[length-1] == "current" else int(row[length-1])
+            people.append((row[name], start, end))
+
+# print(people)
+
+querier = scholar.ScholarQuerier()
+for p in people:
+    q = scholar.SearchScholarQuery()
+    q.set_words("author:\"" + p[0].replace(" ", "+") + "\"")
+    q.set_timeframe(p[1], p[2])
+
+    querier.send_query(q)
+    articles += querier.articles
+
+unique_articles = [a for a in articles if a['title'] in {a['title'] for a in articles}]
+
+querier.articles = articles
+
+scholar.txt(querier, False)
+
